12e27359438a15b37927c65431348a2fa5e2961b
Howard Yeh <howard@metacircus.com>
2015-08-08 14:14:25 +0800

    StackViewDemo

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Howard Yeh on 2015-08-08.
+//  Copyright © 2015 Howard Yeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
